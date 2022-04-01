e1966aafe1594a3aca5cc573d754b6ddcf2a6e44
greendrop <greendrop.devel@gmail.com>
2022-04-02 08:03:03 +0900

    fix import_sorter

--- lib/ui/news_search/news_search_page.dart
+++ lib/ui/news_search/news_search_page.dart
@@ -5,10 +5,10 @@ import 'package:flutter/material.dart';
 import 'package:breakpoint/breakpoint.dart';
 import 'package:flutter_gen/gen_l10n/l10n.dart';
 import 'package:flutter_hooks/flutter_hooks.dart';
-import 'package:flutter_news_sample/exceptions/app_exception.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 // Project imports:
+import 'package:flutter_news_sample/exceptions/app_exception.dart';
 import 'package:flutter_news_sample/providers/news_search_state_notifier_provider.dart';
 import 'package:flutter_news_sample/ui/news_search/news_search_content.dart';
 import 'package:flutter_news_sample/ui/news_search/news_search_form.dart';
