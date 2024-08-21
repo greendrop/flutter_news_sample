da8dfc02d6da6325056a69426cb8c3f7a9ec7527
greendrop <greendrop.devel@gmail.com>
2024-08-21 22:56:18 +0900

    useGoSettingPage のテストコードを追加

---
+++ lib/feature/app_router/riverpod/setting_route_data_provider.g.dart
@@ -0,0 +1,25 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'setting_route_data_provider.dart';
+
+// **************************************************************************
+// RiverpodGenerator
+// **************************************************************************
+
+String _$settingRouteDataHash() => r'4fca3d7d164c5a2c34d8fcbb72bf96d76a443bac';
+
+/// See also [settingRouteData].
+@ProviderFor(settingRouteData)
+final settingRouteDataProvider = AutoDisposeProvider<SettingRouteData>.internal(
+  settingRouteData,
+  name: r'settingRouteDataProvider',
+  debugGetCreateSourceHash: const bool.fromEnvironment('dart.vm.product')
+      ? null
+      : _$settingRouteDataHash,
+  dependencies: null,
+  allTransitiveDependencies: null,
+);
+
+typedef SettingRouteDataRef = AutoDisposeProviderRef<SettingRouteData>;
+// ignore_for_file: type=lint
+// ignore_for_file: subtype_of_sealed_class, invalid_use_of_internal_member, invalid_use_of_visible_for_testing_member
--- lib/feature/setting/hook/use_go_setting_page.dart
+++ lib/feature/setting/hook/use_go_setting_page.dart
@@ -1,6 +1,8 @@
 import 'package:flutter_hooks/flutter_hooks.dart';
 import 'package:flutter_news_sample/feature/app_logger/hook/use_app_logger.dart';
+import 'package:flutter_news_sample/feature/app_router/riverpod/setting_route_data_provider.dart';
 import 'package:flutter_news_sample/feature/app_router/route_data/app_route_data.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
 
 typedef UseGoSettingPageReturn = ({
   void Function() run,
@@ -12,12 +14,13 @@ const String _hookName = 'useGoSettingPage';
 
 UseGoSettingPageReturn useGoSettingPage() {
   final context = useContext();
+  final ref = context as WidgetRef;
   final appLogger = useAppLogger();
 
   final run = useCallback(
     () {
       appLogger.i(['$_hookName#run']);
-      return SettingRouteData().go(context);
+      return ref.read(settingRouteDataProvider).go(context);
     },
     [],
   );
