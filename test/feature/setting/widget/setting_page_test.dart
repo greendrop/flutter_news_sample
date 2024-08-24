f0bd174323fa13cd8ded5d1c4edb6e8c9bb4d43d
greendrop <greendrop.devel@gmail.com>
2024-08-25 06:55:21 +0900

    setting 画面遷移のテストコードを追加

--- test/feature/setting/widget/setting_page_test.dart
+++ test/feature/setting/widget/setting_page_test.dart
@@ -1,9 +1,12 @@
 import 'package:flutter_news_sample/feature/app_logger/riverpod/app_logger.dart';
+import 'package:flutter_news_sample/feature/dev_tool/hook/use_push_dev_tool_page.dart';
+import 'package:flutter_news_sample/feature/locale_setting/hook/use_push_locale_setting_page.dart';
 import 'package:flutter_news_sample/feature/setting/widget/dev_tool_list_tile.dart';
 import 'package:flutter_news_sample/feature/setting/widget/locale_list_tile.dart';
 import 'package:flutter_news_sample/feature/setting/widget/setting_about_list_tile.dart';
 import 'package:flutter_news_sample/feature/setting/widget/setting_page.dart';
 import 'package:flutter_news_sample/feature/setting/widget/theme_list_tile.dart';
+import 'package:flutter_news_sample/feature/theme_setting/hook/use_push_theme_setting_page.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:hooks_riverpod/hooks_riverpod.dart';
 
@@ -13,7 +16,41 @@ import '../../../support/widget/test_material_app.dart';
 void main() {
   group('SettingPage', () {
     group('showDevToolがtrue', () {
-      testWidgets('ListTileが表示されること', (tester) async {
+      testWidgets(
+          [
+            'ListTileが表示されること',
+            'ListTileをタップすると、ページ遷移処理が呼ばれること',
+          ].join(', '), (tester) async {
+        var isTappedLocaleSettingPage = false;
+        var isTappedThemeSettingPage = false;
+        var isTappedDevToolPage = false;
+        UsePushLocaleSettingPageReturn usePushLocaleSettingPage() {
+          Future<void> run() {
+            isTappedLocaleSettingPage = true;
+            return Future.value();
+          }
+
+          return (run: run);
+        }
+
+        UsePushThemeSettingPageReturn usePushThemeSettingPage() {
+          Future<void> run() {
+            isTappedThemeSettingPage = true;
+            return Future.value();
+          }
+
+          return (run: run);
+        }
+
+        UsePushDevToolPageReturn usePushDevToolPage() {
+          Future<void> run() {
+            isTappedDevToolPage = true;
+            return Future.value();
+          }
+
+          return (run: run);
+        }
+
         await tester.runAsync(() async {
           await tester.pumpWidget(
             ProviderScope(
@@ -21,7 +58,12 @@ void main() {
                 appLoggerProvider.overrideWithValue(buildAppTestLogger()),
               ],
               child: TestMaterialApp(
-                child: SettingPage(showDevTool: true),
+                child: SettingPage(
+                  usePushLocaleSettingPage: usePushLocaleSettingPage,
+                  usePushThemeSettingPage: usePushThemeSettingPage,
+                  usePushDevToolPage: usePushDevToolPage,
+                  showDevTool: true,
+                ),
               ),
             ),
           );
@@ -32,6 +74,18 @@ void main() {
         expect(find.byType(ThemeListTile), findsOneWidget);
         expect(find.byType(SettingAboutListTile), findsOneWidget);
         expect(find.byType(DevToolListTile), findsOneWidget);
+
+        await tester.tap(find.byType(LocaleListTile));
+        await tester.pumpAndSettle();
+        expect(isTappedLocaleSettingPage, isTrue);
+
+        await tester.tap(find.byType(ThemeListTile));
+        await tester.pumpAndSettle();
+        expect(isTappedThemeSettingPage, isTrue);
+
+        await tester.tap(find.byType(DevToolListTile));
+        await tester.pumpAndSettle();
+        expect(isTappedDevToolPage, isTrue);
       });
     });
 
