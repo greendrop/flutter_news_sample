2c575a6fe0cc325e392b03a566e522f7ae11bbbe
greendrop <greendrop.devel@gmail.com>
2024-08-25 14:13:10 +0900

    useSahre のテストコードを追加

---
+++ test/feature/share/hook/use_share_test.dart
@@ -0,0 +1,60 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_news_sample/feature/app_logger/riverpod/app_logger.dart';
+import 'package:flutter_news_sample/feature/share/hook/use_share.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+import 'package:mocktail/mocktail.dart';
+import 'package:plugin_platform_interface/plugin_platform_interface.dart';
+import 'package:share_plus_platform_interface/method_channel/method_channel_share.dart';
+import 'package:share_plus_platform_interface/share_plus_platform_interface.dart';
+
+import '../../../support/logger.dart';
+import '../../../support/widget/test_material_app.dart';
+
+class MockMethodChannelShare extends Mock
+    with MockPlatformInterfaceMixin
+    implements MethodChannelShare {}
+
+void main() {
+  late MockMethodChannelShare mockMethodChannelShare;
+
+  setUp(() {
+    mockMethodChannelShare = MockMethodChannelShare();
+    SharePlatform.instance = mockMethodChannelShare;
+  });
+
+  group('useShare', () {
+    group('#run', () {
+      testWidgets('Share.shareが呼ばれること', (tester) async {
+        late UseShareReturn share;
+
+        await tester.pumpWidget(
+          ProviderScope(
+            overrides: [
+              appLoggerProvider.overrideWithValue(buildAppTestLogger()),
+            ],
+            child: TestMaterialApp(
+              child: HookConsumer(
+                builder: (context, ref, child) {
+                  share = useShare();
+                  return Container();
+                },
+              ),
+            ),
+          ),
+        );
+        await tester.pumpAndSettle();
+
+        when(() => mockMethodChannelShare.share('Share text')).thenAnswer(
+          (_) async => const ShareResult('', ShareResultStatus.success),
+        );
+
+        await share.run('Share text');
+
+        verify(
+          () => mockMethodChannelShare.share('Share text'),
+        ).called(1);
+      });
+    });
+  });
+}
