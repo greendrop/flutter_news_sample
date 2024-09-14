301d31bcee1ee573f9bbc0ddfec0a4e238014318
greendrop <greendrop.devel@gmail.com>
2024-09-14 16:18:53 +0900

    widgetbook を追加

--- widgetbook/lib/main.dart
+++ widgetbook/lib/main.dart
@@ -1,20 +1,45 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_news_sample/config/theme/app_theme_data.dart';
+import 'package:widgetbook/widgetbook.dart';
+import 'package:widgetbook_annotation/widgetbook_annotation.dart' as widgetbook;
+
+import 'main.directories.g.dart';
 
 void main() {
-  runApp(const MainApp());
+  runApp(const WidgetbookApp());
 }
 
-class MainApp extends StatelessWidget {
-  const MainApp({super.key});
+@widgetbook.App()
+class WidgetbookApp extends StatelessWidget {
+  const WidgetbookApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+    return Widgetbook.material(
+      directories: directories,
+      addons: [
+        DeviceFrameAddon(
+          devices: [
+            ...Devices.ios.all,
+            ...Devices.android.all,
+          ],
+          initialDevice: Devices.ios.iPhone13,
         ),
-      ),
+        InspectorAddon(),
+        MaterialThemeAddon(
+          themes: [
+            WidgetbookTheme(name: 'Light', data: AppThemeData().light),
+            WidgetbookTheme(name: 'Dark', data: AppThemeData().dark),
+          ],
+        ),
+        AlignmentAddon(),
+        BuilderAddon(
+          name: 'SafeArea',
+          builder: (_, child) => SafeArea(
+            child: child,
+          ),
+        ),
+      ],
     );
   }
 }
---
+++ widgetbook/lib/ui/foundation/typography.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:widgetbook_annotation/widgetbook_annotation.dart';
+
+class TextWidget extends StatelessWidget {
+  const TextWidget({
+    super.key,
+    required this.name,
+    this.style,
+  });
+
+  final String name;
+  final TextStyle? style;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(name, style: style);
+  }
+}
+
+class Typography extends StatelessWidget {
+  const Typography({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    const spacing = 16.0;
+    final styles = Theme.of(context).textTheme;
+    return SingleChildScrollView(
+      child: Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          TextWidget(name: 'Display Large', style: styles.displayLarge),
+          TextWidget(name: 'Display Medium', style: styles.displayMedium),
+          TextWidget(name: 'Display Small', style: styles.displaySmall),
+          const SizedBox(height: spacing),
+          TextWidget(name: 'Headline Large', style: styles.headlineLarge),
+          TextWidget(name: 'Headline Medium', style: styles.headlineMedium),
+          TextWidget(name: 'Headline Small', style: styles.headlineSmall),
+          const SizedBox(height: spacing),
+          TextWidget(name: 'Title Large', style: styles.titleLarge),
+          TextWidget(name: 'Title Medium', style: styles.titleMedium),
+          TextWidget(name: 'Title Small', style: styles.titleSmall),
+          const SizedBox(height: spacing),
+          TextWidget(name: 'Body Large', style: styles.bodyLarge),
+          TextWidget(name: 'Body Medium', style: styles.bodyMedium),
+          TextWidget(name: 'Body Small', style: styles.bodySmall),
+          const SizedBox(height: spacing),
+          TextWidget(name: 'Label Large', style: styles.labelLarge),
+          TextWidget(name: 'Label Medium', style: styles.labelMedium),
+          TextWidget(name: 'Label Small', style: styles.labelSmall),
+          const SizedBox(height: spacing),
+          const TextWidget(name: 'Default'),
+        ],
+      ),
+    );
+  }
+}
+
+@UseCase(
+  name: 'Default',
+  type: Typography,
+)
+Widget buildDesignSystemTextStylesUseCase(BuildContext context) {
+  return const Typography();
+}
