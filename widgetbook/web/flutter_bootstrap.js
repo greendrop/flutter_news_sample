6901a0b230f7f21089835c121f3907dba3935176
greendrop <greendrop.devel@gmail.com>
2024-09-21 07:54:06 +0900

    widgetbook ローディング中のテキストを追加

---
+++ widgetbook/web/flutter_bootstrap.js
@@ -0,0 +1,16 @@
+// Ref: https://docs.flutter.dev/platform-integration/web/initialization#example-display-a-progress-indicator
+{{flutter_js}}
+{{flutter_build_config}}
+
+const loading = document.createElement('div');
+document.body.appendChild(loading);
+loading.textContent = "Loading Entrypoint...";
+_flutter.loader.load({
+  onEntrypointLoaded: async function(engineInitializer) {
+    loading.textContent = "Initializing engine...";
+    const appRunner = await engineInitializer.initializeEngine();
+
+    loading.textContent = "Running app...";
+    await appRunner.runApp();
+  }
+});
