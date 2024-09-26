83f86a44195244f106444d020340ef0b2b44ef5a
greendrop <greendrop.devel@gmail.com>
2024-09-26 23:54:12 +0900

    golden test widget の横幅を指定

--- test/support/alchemist/golden_test_widget_scenario.dart
+++ test/support/alchemist/golden_test_widget_scenario.dart
@@ -7,22 +7,27 @@ class GoldenTestWidgetScenario extends StatelessWidget {
     required this.name,
     required this.brightness,
     required this.builder,
+    this.width = 400,
   });
 
   final String name;
   final Brightness brightness;
   final ValueGetter<Widget> builder;
+  final double? width;
 
   @override
   Widget build(BuildContext context) {
     return GoldenTestScenario(
       name: name,
       child: ClipRect(
-        child: MediaQuery(
-          data: MediaQuery.of(context).copyWith(
-            platformBrightness: brightness,
+        child: SizedBox(
+          width: width,
+          child: MediaQuery(
+            data: MediaQuery.of(context).copyWith(
+              platformBrightness: brightness,
+            ),
+            child: builder(),
           ),
-          child: builder(),
         ),
       ),
     );
