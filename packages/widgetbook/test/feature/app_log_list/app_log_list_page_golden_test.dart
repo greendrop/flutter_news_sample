94f2678136401d78435106b04178eab455132f26
greendrop <greendrop.devel@gmail.com>
2025-04-16 09:13:22 +0900

    feat: AppLogListPageのゴールデンテストを追加
    - AppLogListPageにstopLoadingIndicatorプロパティを追加
    - AppLogListPageのデフォルト、ローディング、空状態のビルダー関数を実装
    - AppLogListPageのゴールデンテストを追加
    - ゴールデンテスト用の画像ファイルを追加

---
+++ packages/widgetbook/test/feature/app_log_list/app_log_list_page_golden_test.dart
@@ -0,0 +1,85 @@
+import 'dart:io';
+
+import 'package:alchemist/alchemist.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:widgetbook_workspace/feature/app_log_list/app_log_list_page.dart';
+
+import '../../support/alchemist/golden_test_device_scenario.dart';
+import '../../support/widget/test_material_app.dart';
+
+void main() {
+  group('AppLogListPage Golden Test', () {
+    final fileNamePrefix = 'app_log_list_page${Platform.pathSeparator}';
+    final devices = Device.all;
+
+    goldenTest(
+      'Default',
+      fileName: '${fileNamePrefix}default',
+      builder: () {
+        final children = <Widget>[];
+        for (final device in devices) {
+          children.add(
+            GoldenTestDeviceScenario(
+              name: device.name,
+              device: device,
+              builder: (context) {
+                return TestMaterialApp(
+                  child: buildAppLogListPageDefault(context),
+                );
+              },
+            ),
+          );
+        }
+        return GoldenTestGroup(columns: devices.length, children: children);
+      },
+    );
+
+    goldenTest(
+      'Empty',
+      fileName: '${fileNamePrefix}empty',
+      builder: () {
+        final children = <Widget>[];
+        for (final device in devices) {
+          children.add(
+            GoldenTestDeviceScenario(
+              name: device.name,
+              device: device,
+              builder: (context) {
+                return TestMaterialApp(
+                  child: buildAppLogListPageEmpty(context),
+                );
+              },
+            ),
+          );
+        }
+        return GoldenTestGroup(columns: devices.length, children: children);
+      },
+    );
+
+    goldenTest(
+      'Loading',
+      fileName: '${fileNamePrefix}loading',
+      builder: () {
+        final children = <Widget>[];
+        for (final device in devices) {
+          children.add(
+            GoldenTestDeviceScenario(
+              name: device.name,
+              device: device,
+              builder: (context) {
+                return TestMaterialApp(
+                  child: buildAppLogListPageLoading(
+                    context,
+                    stopLoadingIndicator: true,
+                  ),
+                );
+              },
+            ),
+          );
+        }
+        return GoldenTestGroup(columns: devices.length, children: children);
+      },
+    );
+  });
+}
