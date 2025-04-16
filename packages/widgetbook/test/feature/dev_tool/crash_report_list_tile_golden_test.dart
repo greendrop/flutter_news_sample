df8eb6e50dd718ed9dde06ff5e63afedb8dd8979
greendrop <greendrop.devel@gmail.com>
2025-04-16 09:47:59 +0900

    feat: CrashReportListTileのゴールデンテストを追加
    - CrashReportListTileの有効/無効状態のゴールデンテストを新規作成
    - buildCrashReportListTileEnabledおよびbuildCrashReportListTileDisabled関数を追加
    - ゴールデンテスト用の画像ファイルを追加

---
+++ packages/widgetbook/test/feature/dev_tool/crash_report_list_tile_golden_test.dart
@@ -0,0 +1,66 @@
+import 'dart:io';
+
+import 'package:alchemist/alchemist.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:widgetbook_workspace/feature/dev_tool/crash_report_test_list_tile.dart';
+
+import '../../support/alchemist/golden_test_widget_scenario.dart';
+import '../../support/widget/test_material_app.dart';
+
+void main() {
+  group('CrashReportListTile Golden Test', () {
+    final fileNamePrefix = 'crash_report_list_tile${Platform.pathSeparator}';
+    const brightnesses = Brightness.values;
+
+    goldenTest(
+      'Enabled',
+      fileName: '${fileNamePrefix}enabled',
+      builder: () {
+        final children = <Widget>[];
+        for (final brightness in brightnesses) {
+          children.add(
+            GoldenTestWidgetScenario(
+              name: brightness.name,
+              brightness: brightness,
+              builder: (context) {
+                return TestMaterialApp(
+                  child: buildCrashReportListTileEnabled(context),
+                );
+              },
+            ),
+          );
+        }
+        return GoldenTestGroup(
+          columns: brightnesses.length,
+          children: children,
+        );
+      },
+    );
+
+    goldenTest(
+      'Disabled',
+      fileName: '${fileNamePrefix}disabled',
+      builder: () {
+        final children = <Widget>[];
+        for (final brightness in brightnesses) {
+          children.add(
+            GoldenTestWidgetScenario(
+              name: brightness.name,
+              brightness: brightness,
+              builder: (context) {
+                return TestMaterialApp(
+                  child: buildCrashReportListTileDisabled(context),
+                );
+              },
+            ),
+          );
+        }
+        return GoldenTestGroup(
+          columns: brightnesses.length,
+          children: children,
+        );
+      },
+    );
+  });
+}
