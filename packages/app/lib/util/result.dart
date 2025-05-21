8f115ed49996c7cdb7eccaa6f43a4589067c3b12
greendrop <greendrop.devel@gmail.com>
2025-05-21 11:33:32 +0900

    feat: share_plusを直接使用しないように、ShareHandlerを追加
    - use_share.dart: ShareHandlerを使用してシェア機能を実装し、結果をResult型で返すように変更
    - share_handler.dart: ShareHandlerクラスを新規作成し、シェア処理をResult型でラップ
    - result.dart: Resultクラスを新規作成し、成功とエラーの状態を管理
    - share_handler_test.dart: ShareHandlerのテストを追加し、正常系と例外系の動作を検証
    - news_article_detail.dart: シェア機能の結果をResult型で処理するように変更

---
+++ packages/app/lib/util/result.dart
@@ -0,0 +1,25 @@
+sealed class Result<T> {
+  const Result();
+
+  const factory Result.ok(T value) = Ok._;
+
+  const factory Result.error(Exception error) = Error._;
+}
+
+final class Ok<T> extends Result<T> {
+  const Ok._(this.value);
+
+  final T value;
+
+  @override
+  String toString() => 'Result<$T>.ok($value)';
+}
+
+final class Error<T> extends Result<T> {
+  const Error._(this.error);
+
+  final Exception error;
+
+  @override
+  String toString() => 'Result<$T>.error($error)';
+}
--- packages/widgetbook/lib/feature/news_article_detail/news_article_detail.dart
+++ packages/widgetbook/lib/feature/news_article_detail/news_article_detail.dart
@@ -1,8 +1,8 @@
 import 'package:app/feature/news_article_detail/widget/news_article_detail_page.dart'
     as feature;
 import 'package:app/feature/share/hook/use_share.dart';
+import 'package:app/util/result.dart';
 import 'package:flutter/material.dart';
-import 'package:share_plus/share_plus.dart';
 import 'package:widgetbook_annotation/widgetbook_annotation.dart';
 
 class NewsArticleDetailPage extends StatelessWidget {
@@ -11,8 +11,10 @@ class NewsArticleDetailPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     UseShareReturn useShare() {
-      Future<ShareResult> run(ShareParams params) {
-        return Future.value(const ShareResult('', ShareResultStatus.success));
+      Future<Result<ShareResult>> run(ShareParams params) {
+        return Future.value(
+          const Result.ok(ShareResult('', ShareResultStatus.success)),
+        );
       }
 
       return (run: run);
